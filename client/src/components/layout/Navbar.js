340f901c2526b3fe2d7de3aeac34b504e8178191
Angel Rodriguez <angel-antonio@live.com>
2018-07-13 02:30:21 -0400

    started working on changin from Bootstrap to Material-Ui, currently on NavBar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,9 +3,72 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
-import { clearCurrentProfile } from "../../actions/profileActions";
+import {
+  clearCurrentProfile,
+  getCurrentProfile
+} from "../../actions/profileActions";
+import compose from "recompose/compose";
+
+//Material imports
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuList from "@material-ui/core/MenuList";
+import Button from "@material-ui/core/Button";
+
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ComponentProperty from "./ComponentProperty";
+
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flex: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: null,
+      profile: {}
+    };
+
+    this.onLogoutClick = this.onLogoutClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.getCurrentProfile();
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.profile) {
+      this.setState({ profile: nextProps.profile.profile });
+    }
+  }
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   onLogoutClick(e) {
     e.preventDefault();
     this.props.clearCurrentProfile();
@@ -13,97 +76,122 @@ class Navbar extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     const { isAuthenticated, user } = this.props.auth;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+    const { profile } = this.state;
 
-    const authlinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/feed">
-            Post Feed
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/dashboard">
-            Dashboard
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a
-            href=""
-            onClick={this.onLogoutClick.bind(this)}
-            className="nav-link"
-          >
-            <img
-              className="rounded-circle"
-              src={user.avatar}
-              alt={user.name}
-              style={{ width: "25px", marginRight: "5px" }}
-              title="You must have a gravatar connected to your email to display an image"
-            />{" "}
-            Logout
-          </a>
-        </li>
-      </ul>
-    );
+    function ListItemLinkProfile(props) {
+      const { primary } = props;
 
-    const guestlinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/register">
-            Sign Up
-          </Link>
+      return (
+        <li>
+          <MenuItem button component={Link} to={`/profile/${profile.handle}`}>
+            <ListItemText primary={primary} />
+          </MenuItem>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">
-            Login
-          </Link>
+      );
+    }
+
+    function MenuItemLink(props) {
+      const { primary, onClick } = props;
+      return (
+        <li>
+          <MenuItem button onClick={onClick}>
+            <ListItemText primary={primary} />
+          </MenuItem>
         </li>
-      </ul>
-    );
+      );
+    }
 
     return (
       //class is now className - basics
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            Tetoka
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#mobile-nav"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
+      <div className={classes.root}>
+        <AppBar color="#ffff00" position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="title"
+              color="inherit"
+              className={classes.flex}
+            >
+              Title
+            </Typography>
+            {isAuthenticated ? (
+              <div>
+                <IconButton
+                  aria-owns={open ? "menu-appbar" : null}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <div>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right"
+                    }}
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right"
+                    }}
+                    open={open}
+                    onClose={this.handleClose}
+                  >
+                    <ListItemLinkProfile primary="Profile" />
 
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/profiles">
-                  {" "}
-                  Members
-                </Link>
-              </li>
-            </ul>
-            {isAuthenticated ? authlinks : guestlinks}
-          </div>
-        </div>
-      </nav>
+                    <MenuItemLink
+                      primary="Log out"
+                      onClick={this.onLogoutClick}
+                    />
+                  </Menu>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <Button className={classes.button} href="/login">
+                  <Typography>Login</Typography>
+                </Button>
+                <Button href="/register">
+                  <Typography>Sign Up</Typography>
+                </Button>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
 
 Navbar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
+  profile: state.profile
 });
 
-export default connect(
-  mapStateToProps,
-  { logoutUser, clearCurrentProfile }
+export default compose(
+  withStyles(styles),
+  connect(
+    mapStateToProps,
+    { logoutUser, clearCurrentProfile, getCurrentProfile }
+  )
 )(Navbar);
