8fd7b8758058917489e23ec4450704674ade36dc
Angel Rodriguez <angel-antonio@live.com>
2018-07-22 22:44:07 -0400

    fixed tests up a bit

--- validation/register.js
+++ validation/register.js
@@ -9,19 +9,20 @@ module.exports = function validateRegisterInput(data) {
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-  if (!Validator.isLength(data.name, { min: 2, max: 30 }) && data.name > 0) {
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
   }
 
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
   if (Validator.isEmpty(data.email)) {
     errors.email = "Email field is required";
   }
-  if (!Validator.isEmail(data.email) && data.email > 0) {
-    errors.email = "Email is invalid";
-  }
+  
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }
--- validation/register.test.js
+++ validation/register.test.js
@@ -1,38 +1,59 @@
 const register = require("./register");
 const isEmpty = require("./is-empty");
 
+let data = {name: null, email: null, password: null, password2: null};
+
+beforeEach(() => {
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+})
+
 test("Test that function exists", () => {
-  expect(register()).toBeDefined();
+  expect(register(data)).toBeDefined();
 });
 
-test("Test that we get isValid with inputs", () => {
+test("We get isValid with inputs", () => {
   const data = {
     name: "Angel Antonio",
     email: "angel-antonio@live.com",
     password: "password",
     password2: "password"
   };
-  data.name = !isEmpty(data.name) ? data.name : "";
-  data.email = !isEmpty(data.email) ? data.email : "";
-  data.password = !isEmpty(data.password) ? data.password : "";
-  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+  
 
   expect(register(data)).toEqual({ errors: {}, isValid: true });
 });
 
-test("Test that we get !isValid with wrong inputs", () => {
-  const data = {};
-  data.name = !isEmpty(data.name) ? data.name : "";
-  data.email = !isEmpty(data.email) ? data.email : "";
-  data.password = !isEmpty(data.password) ? data.password : "";
-  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
-
+test("We get !isValid with no inputs", () => {
+  
+  
   expect(register(data)).toEqual({
     errors: {
       name: "Name field is required",
       email: "Email field is required",
       password: "Password field is required",
       password2: "Confirm Password field is required"
+    },
+    isValid: false
+  });
+});
+
+test("We get !isValid with WRONG inputs", () => {
+  const data = {
+    name: "A",
+    email: "antonio@bla",
+    password: "12345",
+    password2: "54321"
+  }
+  
+  expect(register(data)).toEqual({
+    errors: {
+      name: "Name must be between 2 and 30 characters",
+      email: "Email is invalid",
+      password: "Password must be at least 6 characters",
+      password2: "Passwords must match"
     },
     isValid: false
   });
